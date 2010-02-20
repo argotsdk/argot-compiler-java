b50960472ee79c55932ad4c8ded0fc6c783e44d2
David Ryan <david@livemedia.com.au>
2010-02-20 22:56:30 +0000

    Update jar filename.

--- bin/ac.bat
+++ bin/ac.bat
@@ -2,17 +2,5 @@
 rem The Argot Compiler.  Converts .argot files into .dictionary files.
 rem Usage ac.bat <file.argot> [-o <file.dictionary>]
 
-set ARGOT_HOME=%~dp0..
+java -jar %ARGOT_HOME%\lib\argot-compiler-1.3.b.jar %1 %2 %3 %4 %5
 
-if exist "%ARGOT_HOME%\bin\ac.bat" goto gotArgotHome
-echo The ARGOT_HOME environment variable is not defined correctly
-echo Set the environment variable to the base directory.
-
-goto end
-:gotArgotHome
-
-
-
-java -DARGOT_HOME=%ARGOT_HOME% -jar %ARGOT_HOME%\lib\argot-compiler-1.2.0.jar %1 %2 %3 %4 %5
-
-:end
