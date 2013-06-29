2b2543868cabe8219c889e8c90699afe3b756909
David Ryan <david@livemedia.com.au>
2013-06-29 08:11:54 +0000

    No need to prepare file as writing dictionary to memory.

--- src/com/argot/compiler/ArgotCompilerLoader.java
+++ src/com/argot/compiler/ArgotCompilerLoader.java
@@ -5,7 +5,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
@@ -19,21 +18,11 @@ public class ArgotCompilerLoader
 implements TypeLibraryLoader
 {
 	private String _resource;
-	private String _output;
 	private byte[] _dictionaryData;
 	
 	public ArgotCompilerLoader(String resource )
 	{
 		_resource = resource;
-		int index = _resource.lastIndexOf(".");
-		if (index>0)
-		{
-			_output = _resource.substring(0, index) + ".dictionary";
-		}
-		else
-		{
-			_output = _resource + ".dictionary";
-		}
 		_dictionaryData = null;
 	}
 
@@ -97,6 +86,7 @@ implements TypeLibraryLoader
 		catch (ArgotCompilerException e) 
 		{
 			e.printErrors(System.err);
+			throw new TypeException("Failed to load argot source file: " + _resource, e);
 		}
 		
 		try
